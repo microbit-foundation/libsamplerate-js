14bdda3603133d21c07cfb48974617504576a7f7
Matt Hillsdon <matt.hillsdon@microbit.org>
2024-08-28 10:21:26 +0100

    Rebuild with higher quality settings disabled
    This significantly shrinks the bundle size as they contain a large
    amount of data.

--- dist/glue.d.ts
+++ dist/glue.d.ts
@@ -1,2 +1,2 @@
 export default LoadSRC;
-declare function LoadSRC(moduleArg?: {}, ...args: any[]): {};
+declare function LoadSRC(LoadSRC: any, ...args: any[]): any;
--- scripts/library/build_library.sh
+++ scripts/library/build_library.sh
@@ -6,7 +6,9 @@ make clean
 ./autogen.sh 
 emconfigure ./configure \
 --enable-static \
---disable-shared
+--disable-shared \
+--disable-sinc-medium \
+--disable-sinc-best
 emmake make
 cd $cdw
 mv libsamplerate/src/.libs/libsamplerate.a lib/libsamplerate.a
